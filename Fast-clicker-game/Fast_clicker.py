050b32f426d7be319fba042a66c164b5d5110298
oumniasabri <oumniasabri>
2023-10-13 09:23:29 +0100

    Rectangle class

--- Fast-clicker-game/Fast_clicker.py
+++ Fast-clicker-game/Fast_clicker.py
@@ -7,6 +7,11 @@ mw = pygame.display.set_mode((500, 500)) #main window
 mw.fill(back)
 clock = pygame.time.Clock()
 
+'''Rectangle class'''
+class Area():
+  def __init__(self, x=0, y=0, width=10, height=10, color=None):
+      self.rect = pygame.Rect(x, y, width, height) 
+      self.fill_color = color
 
 
 pygame.display.update()
